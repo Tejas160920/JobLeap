0e4df49c713d903a9a64ab6f2bac6c9ba1b74a7a
Tejas160920 <tejasgaikwad16092002@gmail.com>
2025-12-11 17:37:46 -0500

    Fix: Correct auth middleware import path (auth -> authMiddleware)

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 const notificationController = require("../controllers/notificationController");
 
 // All routes require authentication
