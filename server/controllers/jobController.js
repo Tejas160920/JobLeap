03ffacff059cbb13abb0a0c0ddd5501287f10d0b
Tejas160920 <tejasgaikwad16092002@gmail.com>
2025-12-11 19:35:10 -0500

    Revert to sorting by postedAt (most recent first)

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -163,30 +163,13 @@ exports.getJobs = async (req, res) => {
     const skip = (pageNum - 1) * limitNum;
 
     // Execute query with pagination
-    // Sort: GitHub sources first (simplify, speedyapply), then by date
+    // Sort by most recent first (best for showing latest opportunities)
     const [jobs, totalJobs] = await Promise.all([
-      Job.aggregate([
-        { $match: query },
-        {
-          $addFields: {
-            sourcePriority: {
-              $switch: {
-                branches: [
-                  { case: { $eq: ['$source', 'simplify'] }, then: 1 },
-                  { case: { $eq: ['$source', 'speedyapply'] }, then: 2 },
-                  { case: { $eq: ['$source', 'local'] }, then: 3 },
-                  { case: { $eq: ['$source', 'remoteok'] }, then: 4 }
-                ],
-                default: 5
-              }
-            }
-          }
-        },
-        { $sort: { sourcePriority: 1, postedAt: -1 } },
-        { $skip: skip },
-        { $limit: limitNum },
-        { $project: { sourcePriority: 0 } } // Remove the helper field
-      ]),
+      Job.find(query)
+        .sort({ postedAt: -1 })
+        .skip(skip)
+        .limit(limitNum)
+        .lean(),
       Job.countDocuments(query)
     ]);
 
